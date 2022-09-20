5c138a4b0f3d9fe0257d5d256ae57aa847f0b1c4
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-20 22:42:54 +0300

    completed export and import tests

--- src/data/user.js
+++ src/data/user.js
@@ -3,4 +3,8 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
+console.log(image)
+
 // export all three variables as named exports
+export {username, city,image}
+export default image
